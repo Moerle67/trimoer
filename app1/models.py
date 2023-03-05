7480d1949ce75f225290eb8f04cfaa631e836c10
Ingo Mörl <i.moerl@bfw-schoemberg.de>
2023-03-05 19:32:07 +0100

    bug

--- app1/models.py
+++ app1/models.py
@@ -3,9 +3,9 @@ from django.urls import reverse
 
 # Create your models here.
 class Kontakt(models.Model):
-    name = models.CharField(verbose_name("Name"), max_length=250)
-    tele = models.CharField(verbose_name("Telefon"), max_length=50)
-    mail = models.CharField(verbose_name("E-Mail"), max_length=50)
+    name = models.CharField(verbose_name="Name", max_length=250)
+    tele = models.CharField(verbose_name="Telefon", max_length=50)
+    mail = models.CharField(verbose_name="E-Mail", max_length=50)
     def __str__(self):
         return f"{self.name}"
     class Meta:
@@ -14,9 +14,9 @@ class Kontakt(models.Model):
         ordering = ['name']
 
 class Geraeteklasse(models.Model):
-    klasse = models.CharField(verbose_name("Geräteklasse"), max_length=50)
-    beschreibung = models.TextField(verbose_name("Beschreibung"))
-    details = models.TextField(verbose_name("Details"), blank=True)
+    klasse = models.CharField(verbose_name="Geräteklasse", max_length=50)
+    beschreibung = models.TextField(verbose_name="Beschreibung")
+    details = models.TextField(verbose_name="Details", blank=True)
     def __str__(self):
         return f"{self.klasse}"
     class Meta:
@@ -28,7 +28,7 @@ class Ticket(models.Model):
     name = models.CharField(verbose_name="Name", max_length=50)
     beschreibung = models.TextField(verbose_name="Beschreibung")
     klasse = models.ForeignKey(Geraeteklasse, verbose_name=("Geräteklasse"), on_delete=models.CASCADE)
-    erstellt = models.DateTimeField(verbose_name"Erstellt", auto_now=False, auto_now_add=True)
+    erstellt = models.DateTimeField(verbose_name="Erstellt", auto_now=False, auto_now_add=True)
     geaendert = models.DateTimeField(geaendert="", auto_now=True, auto_now_add=False)
     class Meta:
         verbose_name = ("Ticket")
